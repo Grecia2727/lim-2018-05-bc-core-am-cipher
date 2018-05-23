185500a0be03c35ba7b03b4ab7de63a0adb9cbe3
gutierrezanicamalucero@gmail.com <gutierrezanicamalucero@gmail.com>
2018-05-23 17:12:05 -0500

    Subiendo codigo JS y HTML sin CSS

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,31 @@
-window.cipher = {
-  // ... 
+window.cipher = {    //"cipher" es el nombre del objeto, si deseo puedo cambiar de nombre al objeto.
+
+  encode: (offset, string) => {
+      let nuevoTexto = '';
+      let ingresarTexto = string;
+      let key = parseInt(offset);
+      for (i = 0; i < ingresarTexto.length; i++) {
+        let convAscii = ingresarTexto.charCodeAt(i);
+          nuevoTexto += String.fromCharCode((convAscii - 65 + key) % 26 + 65); 
+     }
+     return nuevoTexto;
+     console.log(nuevoTexto)
+  },  
+
+
+  decode: (offset, string) => {
+    let nuevoTexto = '';
+    let ingresarTexto = string;
+    let key = parseInt(offset);
+    for (i = 0; i < ingresarTexto.length; i++) {
+      let convAscii = ingresarTexto.charCodeAt(i);
+        nuevoTexto += String.fromCharCode((convAscii - 65 + key) % 26 + 65); 
+   }
+   return nuevoTexto;
+   console.log(nuevoTexto)
+  }
+
 };
+  cipher.encode('3', 'ABC')
+
+
---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+
+let btnEncode = document.getElementById("botonE");
+let ingTexto = document.getElementById("ingTextoHtml");
+let salTexto = document.getElementById("salTextoHtml");
+
+btnEncode.addEventListener("click", encriptar);
+	function encriptar(){
+	let  llave = document.getElementById("keyHtml").value;
+	salTexto.value=cipher.encode(llave, ingTexto.value);
+};
+
+// IMPORTANTE: 
+// document.getElementById("keyHtml")==> Captura la caja de texto
+// .value   ==> captura el valor de la caja de texto.
+// salTexto ==> es variable de salida, no tiene un valor para capturar.
+// el .value de salTexto ==> es igual al resultado que deseo mostrar. 
+// --------------------------------------------------------
+
+
