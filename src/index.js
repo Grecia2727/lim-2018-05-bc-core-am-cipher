6ccc354ddfef261da8ed4fd410515a7c794aad17
Grecia <gutierrezanicamalucero@gmail.com>
2018-05-24 12:44:39 -0500

    Subiendo Readme y CSS

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ let ingTexto = document.getElementById("ingTextoHtml");
 let salTexto = document.getElementById("salTextoHtml");
 
 btnEncode.addEventListener("click", encriptar);
+// addEventListener es una funcion de JS que tiene 2 parametros. 
+// Uno de tipo String= clic, y encriptar
+
 	function encriptar(){
 	let  llave = document.getElementById("keyHtml").value;
 	salTexto.value=cipher.encode(llave, ingTexto.value);
