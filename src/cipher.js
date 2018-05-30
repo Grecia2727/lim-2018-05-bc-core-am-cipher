bf3a3ee8a53530d4dae71d0b7e9f70b70ef0545d
Grecia <gutierrezanicamalucero@gmail.com>
2018-05-30 10:10:50 -0500

    Pruebas Test; y Responsive

--- src/cipher.js
+++ src/cipher.js
@@ -1,23 +1,21 @@
 window.cipher = {    //"cipher" es el nombre del objeto, si deseo puedo cambiar de nombre al objeto.
 
   encode: (offset, string) => {
+    // console.log(typeof offset);
       let nuevoTexto = '';
       let ingresarTexto = string;
       let key = parseInt(offset);
+      // console.log(typeof key);
       for (let i = 0; i < ingresarTexto.length; i++) {
         let convAscii = ingresarTexto.charCodeAt(i);
         
         if (convAscii >= 65 && convAscii <= 90) {
-          nuevoTexto += String.fromCharCode((convAscii - 65 + key) % 26 + 65); // Mayus
+          nuevoTexto += String.fromCharCode((convAscii - 65 + key) % 26 + 65); // Mayus Ascii: 65 - 90
         } else if (convAscii >= 97 && convAscii <= 122) {
-          nuevoTexto += String.fromCharCode((convAscii - 97 + key) % 26 + 97); // Minus 
-        } else if (convAscii >= 32 && convAscii <= 64)  {
-          nuevoTexto += String.fromCharCode(nuevoTexto); // signos
+          nuevoTexto += String.fromCharCode((convAscii - 97 + key) % 26 + 97); // Minus Ascii: 97 - 122
         }  else {
-          nuevoTexto += String.fromCharCode(nuevoTexto);
+          nuevoTexto= nuevoTexto + ingresarTexto[i]; }
         }
-
-     }
      return nuevoTexto;
     //  console.log(nuevoTexto)
   },  
@@ -30,9 +28,9 @@ window.cipher = {    //"cipher" es el nombre del objeto, si deseo puedo cambiar 
     for (let i = 0; i < ingresarTexto.length; i++) {
       let convAscii = ingresarTexto.charCodeAt(i);
       if (convAscii >= 65 && convAscii <= 90) {
-      nuevoTexto += String.fromCharCode((convAscii - 65 - key + 26 * 2) % 26 + 65);
+      nuevoTexto += String.fromCharCode((convAscii - 65 - key + 26 * 2) % 26 + 65); // Mayus Ascii: 65 - 90
       } else if (convAscii >= 97 && convAscii <= 122) {
-      nuevoTexto += String.fromCharCode((convAscii - 97 - key + 26 * 2) % 26 + 97);
+      nuevoTexto += String.fromCharCode((convAscii - 97 - key + 26 * 2) % 26 + 97); // Minus Ascii: 97 - 122
       } else {
       nuevoTexto += String.fromCharCode(nuevoTexto);
       }
@@ -42,7 +40,8 @@ window.cipher = {    //"cipher" es el nombre del objeto, si deseo puedo cambiar 
    }
 
 };
-  cipher.encode('3', 'ABC')
+//console.log(window.cipher.encode(33,'ABCDEFGHIJKLMNOPQRSTUVWXYZ') ==='HIJKLMNOPQRSTUVWXYZABCDEFG');
+
 
 
 
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,7 +10,10 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33',()=> {
+      assert.equal(cipher.encode(33,'ABCDEFGHIJKLMNOPQRSTUVWXYZ'),'HIJKLMNOPQRSTUVWXYZABCDEFG');
+    });
+     
   });
 
   describe('cipher.decode', () => {
@@ -19,7 +22,9 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
+      
+    });
   });
 
   describe('cipher.createCipherWithOffset', () => {
@@ -29,7 +34,6 @@ describe('cipher', () => {
     });
 
     it('debería retornar un objeto con dos funciones (encode y decode) con offset fijado');
-
   });
 
 });
